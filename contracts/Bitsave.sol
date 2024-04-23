33b9ef02a3503b2359ebcf7148077db5ba44ebbf
xpanvictor <xpanvictor@gmail.com>
2024-04-23 01:58:59 +0100

    feat: constructor initialization

---
+++ contracts/Bitsave.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
+    constructor() ERC721("NFT Name", "NFT") {}
+
+    function mint(address recipient) public returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+}
+
+contract Bitsave {
+
+  // *** Contract parameters ***
+  address public stableCoin;
+  address public masterAddress;
+  uint256 public rewardPool;
+
+  constructor(address _stableCoin) payable {
+    stableCoin = _stableCoin;
+    masterAddress = msg.sender;
+    rewardPool = 0;
+  }
+
+  receive() external payable {}
+
+}
+
